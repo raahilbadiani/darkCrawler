233753c43ddec875d9fbda3f7768f5a1977ec7ca
Raahil2909 <raahilbadiani@gmail.com>
2022-06-12 02:03:31 +0530

    limited routes per website domain and better printing on console

--- a.py
+++ a.py
@@ -1,53 +1,77 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 session = requests.session()
 session.proxies["http"] = "socks5h://localhost:9050"
 session.proxies["https"] = "socks5h://localhost:9050"
 
-active = [] # contains urls yet to be visited 
-visited = {} # contains urls already visited 
+active = [] # contains all urls 
+visited = {} # contains domain names already visited 
 routes = {} # contains different routes for a particular url we can visit 
+count = 0
+activeAdded = {}
 
 def getSeeds():
     # modify it later to get seeds from database using better techniques
-    url = "http://darkzzx4avcsuofgfez5zq75cqc4mprjvfqywo45dfcaxrwqg6qrlfid.onion/"
+    url = "darkzzx4avcsuofgfez5zq75cqc4mprjvfqywo45dfcaxrwqg6qrlfid.onion"
     return [url]
 
 
 def getUrls(parUrl):
-    global active, visited, routes
+    global active, visited, routes, activeAdded, count
 
     if parUrl in visited:
-        return
+        return # already visited domain hence ignore
+
+    visited[parUrl] = True # marking visitd flag to true
 
     if parUrl not in routes:
-        routes[parUrl] = []
-    
-    print(f'printing urls for {parUrl}')
-    try:
-        response = session.get(parUrl)
-        soup = BeautifulSoup(response.content, "html.parser")
+        routes[parUrl] = [''] # initialize as list with empty string i.e '/' route
 
-        visited[parUrl] = True
 
-        for link in soup.find_all("a"):
-            url = link.get("href")
-            if link.get("href").startswith("/"):
-                # /routes of the same domain
-                # also right now not checking unique routes to be done when storing in database
-                routes[parUrl].append(url)
-            else:
-                # new domains
-                # extract domain from this !!
-                print(url)
-                if url not in visited:
-                    active.append(url)
-    except:
-        print(f'[-] Could not visit {parUrl}')
+    count += 1
+    print(f'...............domains visited = {count}............')
+    routeCnt = 0
+    print(f'Domain: {parUrl}')
+
+
+    for route in routes[parUrl]:
+        routeCnt+=1
+        if routeCnt > 10:
+            break # to not overvisit same website and also give others a chance
+            # change this later to make it dyanmic
+
+        print(f'{routeCnt}; {route=}')
+        
+        try:
+            fullPath = f'{parUrl}/{route}'
+            response = session.get(fullPath)
+            soup = BeautifulSoup(response.content, "html.parser")
+
+
+            for link in soup.find_all("a"):
+                url = link.get("href")
+                url = re.sub("#.*","",url) # remove # symbol
+                url = re.sub(".*http(s?)://","",url) # remove http/https from url 
+                urlSplitted = re.split('/',url) # separate domain and route
+                curDomain = urlSplitted[0] # domain name
+                curRoutes = '/'.join(urlSplitted[1:]) # route
+               
+
+                if  curDomain != '' and curDomain not in activeAdded: 
+                    activeAdded[curDomain] = True # means we have hit it once
+                    active.append(curDomain)
+                if curRoutes != '':
+                    routes[parUrl].append(curRoutes)
+                # print(f'{url=}; {curRoutes=}')
+
+
+        except:
+            print(f'[-] Could not visit {parUrl}')
 
 active = getSeeds()
 
 for url in active:
+    url = "http://"+url
     getUrls(url)
-    # print(urls)
